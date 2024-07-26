2d15b87edf87c8cc4d33c14622937cf765796f75
caf <c_arnaez.flores@hotmail.com>
2024-07-26 19:42:48 -0400

    Archivos estaticos

--- mascotas/settings.py
+++ mascotas/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Directorios adicionales donde Django debe buscar archivos estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
