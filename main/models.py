6886791aa0ec8fb1f63f3ce20cdbbbf1d47fc77d
caf <c_arnaez.flores@hotmail.com>
2024-07-30 20:02:23 -0400

    Implementacion modelo Mascota

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=100)
+    raza = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    edad = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("¡Bienvenido a Mascotas!");
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     alert("¡Bienvenido a Mascotas!");
+// });
